c2ba08398a14f783175b9f2e7a7a8257edf9c295
Sean <ttour1912@163.com>
2017-05-27 15:27:12 +0800

    tw: add a skip.

--- app/src/main/java/com/hell/dohard/MainActivity.java
+++ app/src/main/java/com/hell/dohard/MainActivity.java
@@ -1,13 +1,36 @@
 package com.hell.dohard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_go_git)
+    Button btnGoGit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_go_git})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_go_git:
+                Intent intent = new Intent(MainActivity.this, GitIntroActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
