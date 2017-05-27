ff61d074fb095aafb91bdf5f83d0572c4a69fe5d
Sean <ttour1912@163.com>
2017-05-27 14:38:09 +0800

    tw: all of git intro.

---
+++ app/src/main/java/com/hell/dohard/GitIntroActivity.java
@@ -0,0 +1,13 @@
+package com.hell.dohard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitIntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_intro);
+    }
+}
